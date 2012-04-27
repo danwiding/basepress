6561e608b6eaf9882d17d369ff483db93607a543
Dan Widing <daniel.widing@gmail.com>
2012-04-27 13:55:11 -0500

    changing the mysql php migration over to a submodule and changes sensitive config loader to only load files once

--- junto-common/sensitive-config-loader.php
+++ junto-common/sensitive-config-loader.php
@@ -5,12 +5,7 @@ function SensitiveConfigLoader($path){
 	throw new exception("sensitive data file is missing");
 	$sensitiveArray = json_decode($sensitiveJSONDataString, true);
 	foreach ($sensitiveArray as $topKey => $topValue){
-		if(is_array($topValue)){
-			foreach ($sensitiveArrayLevel as $secondaryKey => $secondaryValue){
-				define($secondaryKey, $secondaryValue);
-			}
-		} else {
-			define($topKey, $topValue);
-		}
+        if(!defined($topKey))
+            define($topKey, $topValue);
 	}
 }
