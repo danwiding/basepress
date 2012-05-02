305d308f0354e818952e21b631c7926157b4ed82
Matt <Matt@polymathic.me>
2012-05-02 18:24:08 -0500

    Update poly_baseline

--- junto-common/poly_baseline.php
+++ junto-common/poly_baseline.php
@@ -29,24 +29,25 @@
 
 //**********     Disable WP Admin Bar     **********//
 
-	wp_deregister_script('admin-bar');
-	wp_deregister_style('admin-bar');
+	if (!defined('ALLOW_ADMINBAR')):
+		wp_deregister_script('admin-bar');
+		wp_deregister_style('admin-bar');
+		
+		remove_action('wp_footer','wp_admin_bar_render',1000);
+	 	remove_action('admin_footer', 'wp_admin_bar_render', 1000);
+		remove_action('personal_options', '_admin_bar_preferences');
 	
-	remove_action('wp_footer','wp_admin_bar_render',1000);
- 	remove_action('admin_footer', 'wp_admin_bar_render', 1000);
-	remove_action('personal_options', '_admin_bar_preferences');
-
-    add_filter('admin_head','remove_admin_bar_style_backend');
-    function remove_admin_bar_style_backend(){
-		echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';
-    }
-
-    add_filter('wp_head','remove_admin_bar_style_frontend', 99);		
-    function remove_admin_bar_style_frontend(){ 
-		echo '<style type="text/css" media="screen">
-		html { margin-top: 0px !important; }
-		* html body { margin-top: 0px !important; }
-		</style>';
-    }
+	    add_filter('admin_head','remove_admin_bar_style_backend');
+	    function remove_admin_bar_style_backend(){
+			echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';
+	    }
 	
+	    add_filter('wp_head','remove_admin_bar_style_frontend', 99);		
+	    function remove_admin_bar_style_frontend(){ 
+			echo '<style type="text/css" media="screen">
+			html { margin-top: 0px !important; }
+			* html body { margin-top: 0px !important; }
+			</style>';
+	    }
+	endif;
 ?>
