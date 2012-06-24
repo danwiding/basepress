877c4f8028673954f29cea638141c939800072cc
Matt <Matt@polymathic.me>
2012-06-24 17:06:32 -0500

    Krumo addition to poly_baseline

--- junto-common/poly_baseline.php
+++ junto-common/poly_baseline.php
@@ -1,7 +1,7 @@
 <?php 	//	BASELINE POLYMATHIC FUNCTIONS
 
 //**********     Include View File     **********//
-	
+
 	function views($include, $once = false){
 		$uri = POLY_THEME_PATH.'/'.get_option('template');
 		return ($once ? include_once($uri.'/__views/'.$include) : include($uri.'/__views/'.$include));
@@ -21,8 +21,10 @@
 		}
 	}
 
-//**********     Print Nice     **********//
-	
+//**********     Print Nice / Krumo     **********//
+
+	include_once('junto-content/krumo/class.krumo.php');
+
 	function print_nice($var, $dump = false){
 		echo '<pre>';
 		($dump ? var_dump($var) : print_r($var));
@@ -30,7 +32,7 @@
 	}
 
 //**********     Browser Specific Body Classes     **********//
-	
+
     add_filter('body_class', 'polymathic_browser_body_classes');
     function polymathic_browser_body_classes($classes){
         global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
@@ -55,18 +57,18 @@
 	if (!defined('ALLOW_ADMINBAR')):
 		wp_deregister_script('admin-bar');
 		wp_deregister_style('admin-bar');
-		
+
 		remove_action('wp_footer','wp_admin_bar_render',1000);
 	 	remove_action('admin_footer', 'wp_admin_bar_render', 1000);
 		remove_action('personal_options', '_admin_bar_preferences');
-	
+
 	    add_filter('admin_head','remove_admin_bar_style_backend');
 	    function remove_admin_bar_style_backend(){
 			echo '<style>body.admin-bar #wpcontent, body.admin-bar #adminmenu { padding-top: 0px !important; }</style>';
 	    }
-	
-	    add_filter('wp_head','remove_admin_bar_style_frontend', 99);		
-	    function remove_admin_bar_style_frontend(){ 
+
+	    add_filter('wp_head','remove_admin_bar_style_frontend', 99);
+	    function remove_admin_bar_style_frontend(){
 			echo '<style type="text/css" media="screen">
 			html { margin-top: 0px !important; }
 			* html body { margin-top: 0px !important; }
